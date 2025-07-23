f909b6af5934f68fe6cd26911217540f58980bfe
shahadot99999 <shahadot99999@gmail.com>
2025-07-23 16:48:12 +0600

    Design LeftNavbar and Fetch and Show Category Data

--- src/layouts/HomeLayout.jsx
+++ src/layouts/HomeLayout.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from '../components/Header';
 import LatestNews from '../components/LatestNews';
 import Navbar from '../components/Navbar';
+import LeftNavbar from '../components/layout-components/LeftNavbar';
 
 const HomeLayout = () => {
 return <div className='font-poppins'>
@@ -15,7 +16,9 @@ return <div className='font-poppins'>
      <Navbar></Navbar>
     </nav>
     <main className='w-11/12 mx-auto pt-2 grid  md:grid-cols-12 gap-3'>
-      <aside className='left col-span-3'>Left Navbar</aside>
+      <aside className='left col-span-3'>
+        <LeftNavbar></LeftNavbar>
+      </aside>
       <section className='col-span-6'>Main Content</section>
       <aside className='col-span-3'>Right Navbar</aside>
     </main>
